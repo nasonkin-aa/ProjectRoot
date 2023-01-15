0ff151ea2c8c89c6e9ae67939442bd762e51aa54
nasonkin-aa <game-akaynt@mail.ru>
2023-01-15 12:56:03 +1000

    Add sprites

--- Root/Assets/Scripts/Dialogue/Dialogue.cs
+++ Root/Assets/Scripts/Dialogue/Dialogue.cs
@@ -5,7 +5,6 @@ using UnityEngine;
 [System.Serializable]
 public class Dialogue
 {
-
     public string[] names;
 
     [TextArea(3, 10)]
--- Root/Assets/Scripts/GameEvents.cs
+++ Root/Assets/Scripts/GameEvents.cs
@@ -17,7 +17,6 @@ public class GameEvents : MonoBehaviour
         if (onDialogEventTrigger != null)
         {
             onDialogEventTrigger(id);
-            Debug.Log("1");
         }
     }
 }
